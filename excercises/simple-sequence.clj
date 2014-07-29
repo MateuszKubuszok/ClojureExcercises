f1643be07a7c300c5cb36ec4fe30b987502fd505
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2014-07-29 11:11:18 +0200

    Small excercises to get the feeling of the language

---
+++ excercises/simple-sequence.clj
@@ -0,0 +1,27 @@
+(ns excercises)
+
+(defn
+  ^{:doc  "Exponent for natural powers"
+    :test #(assert (= 32 (exp 2 5)))}
+  exp [x n]
+  (reduce * (repeat n x))) ; generates list of n xs and calculates their product
+
+(defn
+  ^{:doc  "1/(2^n) fraction"
+    :test #(assert (= (/ 1 32) (nth-2-frac 2 5)))}
+  nth-2-frac [n]
+  (/ 1 (exp 2 n))) ; returns 1/(2^n) fraction
+
+(defn
+  ^{:doc  "1/(2^n) fraction lazy sequence"
+    :test #(assert (=
+                     (list 1 (/ 1 2) (/ 1 4) (/ 1 8) (/ 1 16))
+                     (take 5 (nth-2-frac-seq))))}
+  nth-2-frac-seq
+  ([]  (nth-2-frac-seq 0)) ; makes (nth-2-frac-seq) call (nth-2-frac-seq 0)
+  ([n] (cons                            ; appends
+         (nth-2-frac n)                 ; 1/(2^n) in front of
+         (lazy-seq                      ; lazy sequence of
+           (nth-2-frac-seq (inc n)))))) ; nth-2-frac-seq's following elements
+
+(take 40 (nth-2-frac-seq)) ; first 40 elements of 1/(2^n) sequence
---
+++ excercises/synchronization.clj
@@ -0,0 +1,21 @@
+(ns excercises)
+
+(defn initiate-model [] '()) ; initiate model as empty list
+
+(def model (ref (initiate-model))) ; create model
+
+(defn update-model-fun [fun] ; function
+  (fn [mod arg]              ; returning function
+    (fun mod arg)))          ; that calls passed function for arguments
+
+(defn update-model [fun arg]                   ; takes function and arg and
+  (dosync                                      ; runs transaction that will
+    (alter model (update-model-fun fun) arg))) ; update our model with them
+
+; adds 3 values to the model
+(update-model conj 1)
+(update-model conj 2)
+(update-model conj 3)
+
+; sorts model list - ascending order, needed to reverse order of arguments
+(update-model #(sort %2 %1) <)
